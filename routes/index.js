129b82fb2d69df6383433a62ac4be34f503948c6
StellGG <stell_artem@mail.ru>
2023-04-05 18:31:34 +0300

    5 Aprile

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,8 @@ const mailService = require('../service/mail.service')
 /* GET home page. */
 
 router.post('/send', async function(req, res, next) {
-  const body = req.body
-
-
-  console.log(body)
-  // res.json(await mailService.sendActivationMail("stellgame.pro@gmail.com", `/`, body))
+  const {body} = req
+  res.json(await mailService.sendActivationMail("stellgame.pro@gmail.com", `/`, body))
 });
 
 module.exports = router;
